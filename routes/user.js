38fc4ccfe8c85266e1bfed38bf211b9b496a5227
Sudhanshu kumar <sudhanshukr8971@gmail.com>
2025-02-11 23:23:06 +0530

    updating the signup and login route

--- routes/user.js
+++ routes/user.js
@@ -8,16 +8,47 @@ const user_router = express.Router();
 user_router.post("/register",signup,async(req,res)=>{
     try{
        console.log(req.body);
-       res.json({
-        message:"successfull!!"
+        const user = User(req.body)
+        await user.save()
+         res.json({
+        message:"successfull!!",
+        user
        })
     
 
     }catch(e){
-        // console.log(err);
+        console.log(err);
+        res.json({
+        message:"unsuccessfull!!",
+        Error: e.Error
+       })
     }
 
 } )
+
+// login route 
+user_router.post("/login",async(req,res)=>{
+     try{
+        const {email,password }= req.body
+        const user = await User.findbycredential(email,password)
+        const token = await user.generatetoken()
+        res.status(200).json({
+            message:"Verified",
+            token
+        })
+    
+        // console.log({user,token})
+
+     }catch(e){
+        console.log(e);
+        res.status(500).send({
+            message:"unsuccessfull!!",
+            Error: e.Error
+           })
+     }
+})
+
+
 
 
 module.exports= user_router
